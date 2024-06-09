4129193586639e1ff08771764ff034bb55267d07
Gabrielygor <gabrielcanuto042@gmail.com>
2024-06-09 13:18:08 -0300

    ADD js para abrir menu de pesquisa

--- dashboard-Modelo-01/src/js/script.js
+++ dashboard-Modelo-01/src/js/script.js
@@ -2,7 +2,19 @@
 const themeBtn = document.getElementById('theme-btn');
 const body = document.getElementById('body');
 
+const searchDiv = document.getElementById('state-select-toggle');
+const stateIcon = document.getElementById('state-select-toggle__icon')
+const searchList = document.getElementById('state-select-list')
+
+
 themeBtn.addEventListener('click', ()=> {
     document.body.classList.toggle('dark-mode');
-    console.log(body.classList);
 });
+
+
+searchDiv.addEventListener('click',() => {
+    searchList.classList.toggle('state-select-list--show');
+    stateIcon.classList.toggle('state-select-toggle__icon--rotate');
+} )
+
+console.log();
