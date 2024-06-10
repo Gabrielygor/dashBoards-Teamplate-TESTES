5e7bad8a9ae793f5ecdc3e83dabb5ac9f4a3819d
Gabrielygor <gabrielcanuto042@gmail.com>
2024-06-10 15:51:23 -0300

    Add functions de carregar dados da api

--- dashboard-Modelo-01/src/js/apiThingSpeak.js
+++ dashboard-Modelo-01/src/js/apiThingSpeak.js
@@ -2,41 +2,92 @@ const inputOptions = document.querySelectorAll('#state-select-list li');
 const inputText = document.getElementById('state-select-toggle__state-select');
 
 const temperatura = document.getElementById('temperatura-valor')
+const umidade = document.getElementById('umidade-valor')
+const pressao = document.getElementById('pressao-valor')
+const luminosidade = document.getElementById('luminosidade-valor')
+
 
 let optionId = '1293177' //Chave da api do labican (padrao para o site, vai mudar de acordo com a opção escolhidad)
-const numeroDeLeituras = '1'
+
 
 inputOptions.forEach(item => {
     item.addEventListener('click', () => {
         inputText.innerText = item.innerText
         optionId = item.getAttribute('data-id');
-        updateTemperature();
         searchDiv.dispatchEvent(new Event('click'));
-        
+
+        if (optionId == '1293177') {
+            updateData()
+        }else if (optionId == '72539'){
+            updateData2()
+        } else {
+            updateData3()
+        }
+
         console.log(item.innerText);
         console.log(optionId);
     });
 });
 
-function updateTemperature() {
-    const url = `https://api.thingspeak.com/channels/${optionId}/fields/${numeroDeLeituras}/last.json`
+function updateData() {
+    const url = `https://api.thingspeak.com/channels/${optionId}/feeds/last.json`;
     fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        if (data && data.field1) {
+            temperatura.textContent = data.field1;
+            umidade.textContent = data.field2;
+        } else {
+            console.error('Não foi possível obter a temperatura.');
+        }
+    })
+        .catch(error => {
+            console.error('Erro ao recuperar dados', error);
+        });
+        console.log(url);
+    }
+    
+    function updateData2 () {
+        const url = `https://api.thingspeak.com/channels/${optionId}/feeds/last.json`;
+        fetch(url)
         .then(response => response.json())
         .then(data => {
             if (data && data.field1) {
-                document.getElementById('temperatura-valor').textContent = data.field1;
+                temperatura.textContent = data.field1;
+                umidade.textContent = data.field2;
             } else {
                 console.error('Não foi possível obter a temperatura.');
             }
         })
         .catch(error => {
-            console.error('Erro ao recuperar a temperatura:', error);
+            console.error('Erro ao recuperar dados', error);
         });
-
+        console.log(url);
+    }
+    
+    function updateData3 () {
+        const url = `https://api.thingspeak.com/channels/${optionId}/feeds/last.json`;
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.field1) {
+                temperatura.textContent = data.field1;
+                umidade.textContent = data.field2;
+            } else {
+                console.error('Não foi possível obter a temperatura.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao recuperar dados', error);
+        });
         console.log(url);
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
-    updateTemperature();
-    setInterval(updateTemperature, 60000);
+    updateData();
+    setInterval(updateData, 120000);  //Tempo é lido em milesegundos
 });
+
+
+// const url = `https://api.thingspeak.com/channels/${optionId}/fields/${numeroDeLeituras}/last.json`
