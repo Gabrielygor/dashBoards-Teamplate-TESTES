b1d9edc8bbe3ea96be0d50deeb2a40b09a9112a7
Gabrielygor <gabrielcanuto042@gmail.com>
2024-06-11 15:16:51 -0300

    Teste

--- dashboard-Modelo-01/src/js/graphics.js
+++ dashboard-Modelo-01/src/js/graphics.js
@@ -1,7 +1,26 @@
 const tempGafico = document.getElementById('temperatura-grafico')
 
 export async function temperaturaGafico() {
+    const ctx = document.getElementById('temperaturaGrafico');
 
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
 
 
