f72ce6a0d26288ef56de93bbf0653a8bcbc87606
Gabrielygor <gabrielcanuto042@gmail.com>
2024-06-13 14:05:30 -0300

    ALT corrigindo identação

--- dashboard-Modelo-01/src/js/graphics.js
+++ dashboard-Modelo-01/src/js/graphics.js
@@ -41,39 +41,39 @@ export async function temperaturaGrafico() {
 
 export async function umidadeGrafico() {
     fetch(`https://api.thingspeak.com/channels/${optionId}/fields/2.json?results=${quantidadeDeLeituras}`)
-    .then(response => response.json())
-    .then(data => {
-        const umidade = data.feeds.map(feed => parseFloat(feed.field2));
-        const timestamps = data.feeds.map(feed => feed.created_at);
+        .then(response => response.json())
+        .then(data => {
+            const umidade = data.feeds.map(feed => parseFloat(feed.field2));
+            const timestamps = data.feeds.map(feed => feed.created_at);
 
-        var options = {
-            chart: {
-                type: 'line',
-                height: 350
-            },
-            series: [{
-                name: 'Umidade %',
-                data: umidade
-            }],
-            xaxis: {
-                categories: timestamps,
-                type: 'datetime',
-                labels: {
-                    formatter: function (value) {
-                        // Formata o rótulo para mostrar apenas as horas
-                        const date = new Date(value);
-                        const hours = date.getHours();
-                        const minutes = date.getMinutes();
-                        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+            var options = {
+                chart: {
+                    type: 'line',
+                    height: 350
+                },
+                series: [{
+                    name: 'Umidade %',
+                    data: umidade
+                }],
+                xaxis: {
+                    categories: timestamps,
+                    type: 'datetime',
+                    labels: {
+                        formatter: function (value) {
+                            // Formata o rótulo para mostrar apenas as horas
+                            const date = new Date(value);
+                            const hours = date.getHours();
+                            const minutes = date.getMinutes();
+                            return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+                        }
                     }
-                }
-            },
-        };
+                },
+            };
 
-        var chart = new ApexCharts(document.querySelector("#umidadeGrafico"), options);
-        chart.render();
-    })
-    .catch(error => console.error('Erro ao obter dados do ThingSpeak:', error));
+            var chart = new ApexCharts(document.querySelector("#umidadeGrafico"), options);
+            chart.render();
+        })
+        .catch(error => console.error('Erro ao obter dados do ThingSpeak:', error));
 }
 
 export async function pressaoGrafico() {
