1f77e8bc00b354e38f5fd232f484929a99fcf09e
Gabrielygor <gabrielcanuto042@gmail.com>
2024-06-13 14:03:32 -0300

    ALT correção nome de variavel optionID

--- dashboard-Modelo-01/src/js/script.js
+++ dashboard-Modelo-01/src/js/script.js
@@ -5,36 +5,33 @@ const inputOptions = document.querySelectorAll('#state-select-list li');
 const inputText = document.getElementById('state-select-toggle__state-select');
 const searchDiv = document.getElementById('state-select-toggle');
 
-export var optionId = '1293177'
+export let optionId = '1293177'
 
 inputOptions.forEach(item => {
+
     item.addEventListener('click', () => {
         inputText.innerText = item.innerText
         optionId = item.getAttribute('data-id');
         searchDiv.dispatchEvent(new Event('click'));
 
+        cleanGrafico();
+
         if (optionId == '1293177') {
-            cleanGrafico();
             updateData();
             temperaturaGrafico();
             umidadeGrafico();
 
         } else if (optionId == '72539') {
-            cleanGrafico();
             updateData2();
             temperaturaGrafico();
             umidadeGrafico();
 
         } else {
-            cleanGrafico();
             updateData3();
             temperaturaGrafico();
             umidadeGrafico();
 
         }
-
-        console.log(item.innerText);
-        console.log(optionId);
     });
 });
 
@@ -43,10 +40,8 @@ document.addEventListener('DOMContentLoaded', function () {
     temperaturaGrafico();
     umidadeGrafico();
 
-
-    setInterval(updateData, 120000);  //Tempo é lido em milesegundos
-    setInterval(temperaturaGrafico, 120000);  //Tempo é lido em milesegundos
-    setInterval(umidadeGrafico, 120000);  //Tempo é lido em milesegundos
-
+    setInterval(updateData, 180000);  //Tempo é lido em milesegundos
+    setInterval(temperaturaGrafico, 180000);
+    setInterval(umidadeGrafico, 180000);
 });
 
