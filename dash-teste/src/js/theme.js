4a8e889402e226352f16e3cb8a2b67dbfe676f70
Gabrielygor <gabrielcanuto042@gmail.com>
2024-06-17 10:02:29 -0300

    remove dashboard teste

--- dash-teste/src/js/theme.js
+++
@@ -1,8 +0,0 @@
-const body = document.getElementById('body')
-const themeButton = document.getElementById('theme-btn')
-
-themeButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme')
-    console.log(body.classList);
-});
-
